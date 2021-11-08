8e55b1ea4a591fa83d5ee5d5ef51b3e4ffa1aa0e
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-08 18:58:24 +0800

    jwt authentication filter

--- src/main/java/com/kapcb/framework/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/kapcb/framework/security/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,17 @@
 package com.kapcb.framework.security.filter;
 
+import com.kapcb.framework.security.util.JwtTokenUtil;
+import com.kapcb.framework.web.enums.ResultCode;
+import com.kapcb.framework.web.exception.BusinessException;
+import com.kapcb.framework.web.model.result.CommonResult;
+import kapcb.framework.web.constants.enums.StringPool;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -17,11 +29,27 @@ import java.io.IOException;
  * @version 1.0.0
  * @date 2021/11/6 16:20
  */
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public abstract class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    public abstract UserDetailsService getUserDetailService();
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        String method = httpServletRequest.getMethod();
+        String authorization = httpServletRequest.getHeader(StringPool.HTTP_REQUEST_AUTHORIZATION.value());
 
+        if (StringUtils.isNoneBlank(authorization) && authorization.startsWith(StringPool.AUTHORIZATION_BEARER.value())) {
+            String accessToken = authorization.substring(StringPool.AUTHORIZATION_BEARER.value().length());
+            String username = JwtTokenUtil.getUsername(accessToken);
+            if (StringUtils.isBlank(accessToken) && StringUtils.isBlank(username)) {
+                throw new BusinessException(ResultCode.FAILED);
+            }
+            UserDetails userDetails = getUserDetailService().loadUserByUsername(username);
+            if (JwtTokenUtil.validateToken(accessToken, userDetails.getUsername())) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
