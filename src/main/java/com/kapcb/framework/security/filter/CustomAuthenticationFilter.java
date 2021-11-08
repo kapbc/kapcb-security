865891df34840a5c4cd2a12c467033b251147393
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-08 19:15:00 +0800

    authentication

--- src/main/java/com/kapcb/framework/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/kapcb/framework/security/filter/CustomAuthenticationFilter.java
@@ -1,7 +1,22 @@
 package com.kapcb.framework.security.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.kapcb.framework.web.enums.ResultCode;
+import com.kapcb.framework.web.exception.BusinessException;
+import kapcb.framework.web.constants.enums.StringPool;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
 
 /**
  * <a>Title: CustomAuthenticationFilter </a>
@@ -15,4 +30,26 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        if (!HttpMethod.POST.name().equalsIgnoreCase(request.getMethod())) {
+            log.info("request method must be post request");
+            throw new BusinessException(ResultCode.FAILED);
+        }
+        if (MediaType.APPLICATION_JSON_VALUE.equalsIgnoreCase(request.getContentType())) {
+            String username = StringPool.EMPTY_STRING.value();
+            String password = StringPool.EMPTY_STRING.value();
+
+            try (InputStream inputStream = request.getInputStream()) {
+                JSONObject jsonObject = JSON.parseObject(inputStream);
+            } catch (Exception e) {
+                log.error("");
+                throw new BusinessException(ResultCode.FAILED);
+            }
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken();
+            return this.getAuthenticationManager().authenticate(authenticationToken);
+        } else {
+            return super.attemptAuthentication(request, response);
+        }
+    }
 }
