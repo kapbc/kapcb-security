4fdae5d23f08ce3518a16700873f21b504e44fde
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-15 21:25:52 +0800

    grant-type

--- src/main/java/com/kapcb/framework/security/filter/ValidateCodeFilter.java
+++ src/main/java/com/kapcb/framework/security/filter/ValidateCodeFilter.java
@@ -31,6 +31,11 @@ import java.io.IOException;
 @Slf4j
 public class ValidateCodeFilter extends OncePerRequestFilter {
 
+    // 认证类型
+    private static final String GRANT_TYPE = "grant_type";
+    // 密码模式
+    private static final String PASSWORD = "password";
+
     private static AntPathRequestMatcher requestMatcher;
 
     @Resource
@@ -44,7 +49,7 @@ public class ValidateCodeFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         String header = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
-        if (requestMatcher.matches(httpServletRequest) && StringUtils.equals(httpServletRequest.getParameter("password"), "password")) {
+        if (requestMatcher.matches(httpServletRequest) && StringUtils.equals(httpServletRequest.getParameter("grant_type"), "password")) {
             try {
                 validateCode(httpServletRequest);
             } catch (Exception e) {
